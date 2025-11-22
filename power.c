237731058a57bc94759ee072e29c54587648b148
RubabaBinteMahbub <rubababintemahbub2007@gmail.com>
2025-11-22 11:44:59 +0600

    Update power.c

--- power.c
+++ power.c
@@ -4,14 +4,14 @@ int pow=1;
 for(int i=1;i<=b;i++){
 pow*=a;
 }
-printf("Resul = %d\n",pow);
+printf("Result = %d\n",pow);
 return;
 }
 int main() {
   int num,exp;
   printf("Enter the number  : ");
   scanf("%d",&num);
-	printf("Enter the exponent  : ");
+  printf("Enter the exponent  : ");
   scanf("%d",&exp);
   power(num,exp);
 
